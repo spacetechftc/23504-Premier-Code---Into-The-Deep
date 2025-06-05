5df12b4f2f8694b896a5b0cf54731a8ced4b28c6
Fantxa <154910463+Fantxa@users.noreply.github.com>
2025-06-04 21:42:06 -0300

    [2P] 23504-Premire code! With TeleOp

---
+++ TeamCode/src/main/java/Marcone/aa.java
@@ -0,0 +1,105 @@
+package Marcone;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.PathBuilder;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+
+public class aa {
+
+    public static PathBuilder testando = new PathBuilder();
+
+    public static PathChain line1 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(8.500, 112.100, Point.CARTESIAN),
+                            new Point(19.300, 131.600, Point.CARTESIAN)
+                    )
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-25))
+            .setReversed(true)
+            .build();
+
+    public static PathChain line2 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(19.300, 131.600, Point.CARTESIAN),
+                            new Point(26.000, 127.626, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(-25))
+            .build();
+
+    public static PathChain line3 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(26.000, 127.626, Point.CARTESIAN),
+                            new Point(19.300, 131.600, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(-25))
+            .setReversed(true)
+            .build();
+
+    public static PathChain line4 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(19.300, 131.600, Point.CARTESIAN),
+                            new Point(19.300, 131.600, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line5 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(19.300, 131.600, Point.CARTESIAN),
+                            new Point(19.300, 131.600, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(-25))
+            .build();
+
+    public static PathChain line6 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(19.300, 131.600, Point.CARTESIAN),
+                            new Point(33.196, 130.542, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(45))
+            .build();
+
+    public static PathChain line7 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(33.196, 130.542, Point.CARTESIAN),
+                            new Point(19.300, 131.600, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(-25))
+            .build();
+
+    public static PathChain line8 = testando
+            .addPath(
+                    new BezierCurve(
+                            new Point(19.300, 131.600, Point.CARTESIAN),
+                            new Point(64.598, 102.280, Point.CARTESIAN),
+                            new Point(62.579, 97.121, Point.CARTESIAN)
+                    )
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(-25), Math.toRadians(270))
+            .build();
+
+    public static PathChain line9 = testando
+            .addPath(
+                    new BezierLine(
+                            new Point(62.579, 97.121, Point.CARTESIAN),
+                            new Point(19.065, 131.888, Point.CARTESIAN)
+                    )
+            )
+            .setTangentHeadingInterpolation()
+            .setReversed(true)
+            .build();
+}
